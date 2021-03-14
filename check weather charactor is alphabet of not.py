5889e296bea33b861918a1718456cbb67629bf11
chhaya742 <76725368+chhaya742@users.noreply.github.com>
2021-03-14 12:45:15 +0530

    Add files via upload

---
+++ check weather charactor is alphabet of not.py
@@ -0,0 +1,7 @@
+alpha=input("enter the alphabet").lower()
+a=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+if alpha in a :
+    print("it is alphabet")
+else:
+    print("it is not alphabet") 
+
